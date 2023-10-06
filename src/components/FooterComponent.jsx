ea9973a1fa58c967988a0ee61013631bf8ce2669
Otavie <otavieokuoyo@gmail.com>
2023-10-06 21:30:16 +0100

    Update resume

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -12,19 +12,16 @@ export const FooterComponent = () => {
         <Row className="justify-content-center">
             <Col xs="auto">
                 <Nav.Link href='https://github.com/Otavie' target='_blank'>
-                  {/* <FontAwesomeIcon className="social-link" icon={faGithub} style={{ fontSize: "1.5rem", color: "#FFF" }} /> */}
                   <FontAwesomeIcon className="social-link" icon={faGithub} />
                 </Nav.Link>
             </Col>
             <Col xs="auto">
                 <Nav.Link href='https://www.linkedin.com/in/otavie/' target='_blank'>
-                  {/* <FontAwesomeIcon className="social-link" icon={faLinkedin} style={{ fontSize: "1.5rem", color: "#FFF" }} /> */}
                   <FontAwesomeIcon className="social-link" icon={faLinkedin} />
                 </Nav.Link>
             </Col>
             <Col xs="auto">
                 <Nav.Link href='https://twitter.com/LoveOtavie' target='_blank' >
-                  {/* <FontAwesomeIcon className="social-link" icon={faTwitter} style={{ fontSize: "1.5rem", color: "#FFF" }} /> */}
                   <FontAwesomeIcon className="twitter social-link" icon={faTwitter} />
                 </Nav.Link>
             </Col>
@@ -32,7 +29,7 @@ export const FooterComponent = () => {
       </Container>
       <div className="text-center p-3 pb-5">
         <div class="view-resume">
-          <a class="view-resume__link" href="https://drive.google.com/file/d/1uaou6-1UWTr6Hkyk7gE3fAQg1S4Yb0Fu/view?usp=sharing"><span class="view">Resume</span></a>    
+          <a class="view-resume__link" href="https://drive.google.com/file/d/1R4AfIfm1NXsWX951sYa_xejoLmij3yAN/view?usp=sharing" target='_blank' rel='noreferrer'><span class="view">Resume</span></a>    
         </div>
       </div>
     </footer>
